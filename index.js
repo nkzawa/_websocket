7c1d720ed39f6c1323faffe291f84af6270704db
Naoyuki Kanezawa <naoyuki.kanezawa@gmail.com>
2014-11-09 19:17:20 +0900

    implement permessage-defalte

--- index.js
+++ index.js
@@ -32,36 +32,79 @@ function Server(connectionListener) {
       return;
     }
 
-    var key = req.headers['sec-websocket-key'];
-    if (!key) {
-      abortConnection(socket);
-      return;
-    }
-
     var version = parseInt(req.headers['sec-websocket-version'], 10);
     if (13 !== version) {
       abortConnection(socket);
       return;
     }
 
-    key = crypto.createHash('sha1')
+    var key = req.headers['sec-websocket-key'];
+    if (!key) {
+      abortConnection(socket);
+      return;
+    }
+
+    var acceptKey = crypto.createHash('sha1')
       .update(key + '258EAFA5-E914-47DA-95CA-C5AB0DC85B11')
       .digest('base64');
 
-    socket.write([
+    var extensions = parseExtensions(req);
+
+    var _extensions = {};
+    if (extensions['permessage-deflate']) {
+      _extensions['permessage-deflate'] = true;
+    } else if (extensions['x-webkit-deflate-frame']) {
+      _extensions['x-webkit-deflate-frame'] = true;
+    }
+
+    // TODO:
+    //   client_max_window_bits
+    //   server_max_window_bits
+    //   client_no_context_takeover
+    //   server_no_context_takeover
+
+    _extensions = formatExtensions(_extensions);
+
+    var headers = [
       'HTTP/1.1 101 Switching Protocols',
       'Upgrade: websocket',
       'Connection: Upgrade',
-      'Sec-WebSocket-Accept: ' + key
-    ].join('\r\n') + '\r\n\r\n');
+      'Sec-WebSocket-Accept: ' + acceptKey
+    ];
+    if (_extensions) {
+      headers.push('Sec-WebSocket-Extensions: ' + _extensions);
+    }
 
-    self.emit('connection', new Socket(req, socket));
+    socket.write(headers.join('\r\n') + '\r\n\r\n');
+
+    self.emit('connection', new Socket(req, socket, head, extensions));
   });
 }
 
 Server.prototype.listen = function() {
   this._http.listen.apply(this._http, arguments);
 };
+
+function parseExtensions(req) {
+  // e.g.
+  //   Sec-WebSocket-Extensions: permessage-deflate; client_max_window_bits
+  //   Sec-WebSocket-Extensions: x-webkit-deflate-frame
+
+  var extensions = {};
+  req.headers['sec-websocket-extensions'].split(';').forEach(function(extension) {
+    // FIXME: value may be quoted. e.g. x="10"
+    var parts = extension.trim().split('=');
+    extensions[parts[0].toLowerCase()] = parts.length > 1 ? parts[1] : true;
+  });
+  return extensions;
+}
+
+function formatExtensions(extensions) {
+  return Object.keys(extensions).map(function(extension) {
+    var param = extensions[extension];
+    return true === param ? extension : extension + '=' + param;
+  }).join('; ');
+}
 
 function abortConnection(socket) {
   socket.write([
